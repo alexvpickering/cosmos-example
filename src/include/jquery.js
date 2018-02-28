599ab75beca7ea35a711b4844a59e9522d918388
alexvpickering <alexvpickering@gmail.com>
2018-02-28 14:18:05 -0800

    delete unecessary code

--- src/include/jquery.js
+++ src/include/jquery.js
@@ -1,6 +1,4 @@
 import $ from "jquery";
 import DataTables from "datatables.net";
 
-$.DataTables = DataTables;
-
 window.jQuery = window.$ = $;
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "./include/jquery";
-import "datatables.net-dt/css/jquery.dataTables.css";
 
 configure({ adapter: new Adapter() });
